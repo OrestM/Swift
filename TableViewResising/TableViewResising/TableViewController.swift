2af7c94c4ca65ccd392575cf8c70f9104c359979
Orest Mykha <orestmykha.53@gmail.com>
2017-02-09 11:12:14 +0200

    TableViewResizing Mask Cell

---
+++ TableViewResising/TableViewResising/TableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TableViewController.swift
+//  TableViewResising
+//
+//  Created by Orest Mykha on 2/8/17.
+//  Copyright © 2017 Orest Mykha. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.estimatedRowHeight = 100
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")! as UITableViewCell
+        
+        
+        return cell
+    }
+
+}
